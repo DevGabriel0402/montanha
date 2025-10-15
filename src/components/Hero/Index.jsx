2c87933519cada37d8af83e03c5c89285868907a
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-10-15 14:18:25 -0300

    Atualizando Assets

--- src/components/Hero/Index.jsx
+++ src/components/Hero/Index.jsx
@@ -1,4 +1,3 @@
-import Montanha from "../../assets/foto.webp";
 import Maquina from "../../assets/maquina.webp";
 import Piercing from "../../assets/piercing.webp";
 import { Container } from "./Style";
@@ -8,7 +7,11 @@ export const Hero = () => {
     <Container>
       <div className="sombra"></div>
       <div className="container-foto">
-        <img className="montanha" src={Montanha} alt="foto" />
+        <img
+          className="montanha"
+          src="https://res.cloudinary.com/dsqtianpj/image/upload/v1760548632/Montanha-Mobile_11zon_1_mi61ll.jpg"
+          alt="foto"
+        />
         <img
           className="maquina"
           style={{ width: 100, height: "auto" }}
